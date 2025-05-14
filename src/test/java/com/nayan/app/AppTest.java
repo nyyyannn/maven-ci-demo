a8cdc1363ef1fa8de8e22b156727d43e068910b0
Nayan <nayanshekartg@gmail.com>
2025-05-14 20:41:02 +0530

    try

--- src/test/java/com/nayan/app/AppTest.java
+++ src/test/java/com/nayan/app/AppTest.java
@@ -3,7 +3,6 @@ package com.nayan.app;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import junit.assertTrue;
 /**
  * Unit test for simple App.
  */
@@ -12,6 +11,5 @@ public class AppTest
     @Test
     public void testApp()
     {
-        assertTrue(true);
     }
 }
