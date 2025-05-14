84943b87c49039d18b1f28985b7f6ed02b57b822
Nayan <nayanshekartg@gmail.com>
2025-05-14 20:25:56 +0530

    Initial Commit

---
+++ src/test/java/com/nayan/app/AppTest.java
@@ -0,0 +1,19 @@
+package com.nayan.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue(true);
+    }
+}
