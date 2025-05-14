fe64fefc7858cd346ea843153c32d2a3bd1862f5
Nayan <nayanshekartg@gmail.com>
2025-05-14 20:40:03 +0530

    5th try

--- src/test/java/com/nayan/app/AppTest.java
+++ src/test/java/com/nayan/app/AppTest.java
@@ -3,6 +3,7 @@ package com.nayan.app;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import junit.assertTrue;
 /**
  * Unit test for simple App.
  */
