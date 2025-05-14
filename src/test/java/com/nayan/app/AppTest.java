92ed9ec6e7bcd1e058de173577917707048bc78e
Nayan <nayanshekartg@gmail.com>
2025-05-14 20:51:10 +0530

    testing failed test case

--- src/test/java/com/nayan/app/AppTest.java
+++ src/test/java/com/nayan/app/AppTest.java
@@ -1,15 +1,17 @@
 package com.nayan.app;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testingAddition() {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        assertEquals(10, sum);
     }
 }
